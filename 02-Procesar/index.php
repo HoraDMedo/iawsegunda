9df022c3063cc835b984212669dde1c74bf761ad
HoraDMedo <sam_deckstroyer@hotmail.com>
2022-01-20 13:25:37 +0100

    subio

--- 02-Procesar/index.php
+++ 02-Procesar/index.php
@@ -1,23 +1,36 @@
 <?php
-echo "GET:<br>";
-print_r($_GET);
-echo "<br>";
 
-echo "POST:<br>";
-print_r($_POST);
-echo "<br>";
+$bienvenido=true;
+$nombre="";
 
-echo "COOKIES:<br>";
-print_r($_COOKIES);
-echo "<br>";
 
-echo "SESSION:<br>";
-print_r($_SESSION);
-echo "<br>";
 
-echo "SERVER:<br>";
-print_r($_SERVER);
-echo "<br>";
+if(isset($_POST["enviar"])){
+    $bienvenido=false;
+    $nombre=$_POST["nombre"];
+    $segundonombre=$_POST["apellidos"];
+}
+
+
+#echo "GET:<br>";
+#print_r($_GET);
+#echo "<br>";
+
+#echo "POST:<br>";
+#print_r($_POST);
+#echo "<br>";
+
+#echo "COOKIES:<br>";
+#print_r($_COOKIES);
+#echo "<br>";
+
+#echo "SESSION:<br>";
+#print_r($_SESSION);
+#echo "<br>";
+
+#echo "SERVER:<br>";
+#print_r($_SERVER);
+#echo "<br>";
 ?>
 
 
@@ -32,27 +45,35 @@ echo "<br>";
     <title>Formulario</title>
 </head>
 <body>
+
+    <?php if ($bienvenido) { ?>
+
+        <h1>Bienvenido!!!</h1>
+
+    <?php } ?>
+
+
     <div>
 
-        <form action="index.php" method="post">
+        <form action="index.php" method="POST">
 
             <fieldset>
                 <legend>Información Personal</legend>
                 <label for="nombre">Nombre:</label>
-                <input name="nombre" id="nombre" type="text" tabindex="1" />
+                <input name="nombre" id="nombre" type="text" tabindex="1" value="<?php echo $nombre ;?>"/>
                 <label for="apellidos">Apellidos:</label>
-                <input name="apellidos" id="apellidos" type="text" tabindex="2" />
+                <input name="apellidos" id="apellidos" type="text" tabindex="2" value="<?php echo $segundonombre ;?>"/>
             </fieldset>
 
             <fieldset>
                 <legend>Edad</legend>
-                <label><input type="radio" tabindex="20" name="edad" value="edad" /> 20-39</label>
-                <label><input type="radio" tabindex="21" name="edad" value="edad" /> 40-59</label>
-                <label><input type="radio" tabindex="22" name="edad" value="edad" /> 60-79</label>
+                <label><input type="radio" tabindex="20" value="20-39" name="edad" /> 20-39</label>
+                <label><input type="radio" tabindex="21" value="40-59" name="edad" /> 40-59</label>
+                <label><input type="radio" tabindex="22" value="60-79" name="edad" /> 60-79</label>
             </fieldset>
 <br>
 
-            <input type="submit" name="envssiar" value="Enviar">
+            <input type="submit" name="enviar" value="enviar">
 
         </form>
     </div>
